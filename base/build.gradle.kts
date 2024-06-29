e6a5ced33cd06e7f037b1d00a201f59d2cd7b834
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-06-29 21:27:42 +0200

    added shadowjar

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.jvm)
     alias(libs.plugins.serialization)
+    alias(libs.plugins.shadow)
 }
 
 repositories {
@@ -9,5 +10,32 @@ repositories {
 
 dependencies {
     implementation(libs.minestom)
-    implementation(libs.bundles.serialization)
+    compileOnly(libs.bundles.serialization)
+    compileOnly(libs.coroutines)
+    implementation(libs.bundles.ktor)
+}
+
+tasks.shadowJar {
+    /*
+    fun reloc(packageName: String) {
+        relocate(packageName, "de.airblocks.teamfive.base.dependency.$packageName")
+    }
+
+    reloc("io.ktor")
+     */
+
+    archiveFileName = "base.jar"
+    manifest {
+        attributes(
+            mapOf(
+                "Main-Class" to "de.airblocks.teamfive.base.EntrypointKt"
+            )
+        )
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
