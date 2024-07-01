d20f9b8792398ab55522fd649a156e48df75581a
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-07-01 19:51:06 +0200

    fixed publishing

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -2,20 +2,10 @@ plugins {
     alias(libs.plugins.jvm)
     alias(libs.plugins.serialization)
     alias(libs.plugins.shadow)
-    `maven-publish`
 }
 
 repositories {
     mavenCentral()
-
-    maven {
-        name = "GitHubPackages"
-        url = uri("https://maven.pkg.github.com/OWNER/REPO")
-        credentials {
-            username = project.findProperty("gpr.user") as String?
-            password = project.findProperty("gpr.token") as String?
-        }
-    }
 }
 
 dependencies {
@@ -48,21 +38,5 @@ tasks.shadowJar {
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("baseGitHub") {
-            from(components["java"])
-
-            groupId = groupId
-            artifactId = "base"
-            version = version
-
-            pom {
-                name.set("Base")
-            }
-        }
     }
 }
