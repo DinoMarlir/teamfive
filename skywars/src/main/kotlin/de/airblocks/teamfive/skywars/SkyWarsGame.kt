a040390fc1d92647aa2fc00d792ec713d28102f7
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-07-01 16:37:05 +0200

    added dynamic team sizes

--- skywars/src/main/kotlin/de/airblocks/teamfive/skywars/SkyWarsGame.kt
+++ skywars/src/main/kotlin/de/airblocks/teamfive/skywars/SkyWarsGame.kt
@@ -5,12 +5,15 @@ import de.airblocks.teamfive.base.server.GameServer
 import net.kyori.adventure.text.Component
 
 class SkyWarsGame(
-    override val simpleName: String = "SkyWars",
-    override val name: Component = Component.text(simpleName),
-    override val description: Component = Component.text("The SkyWars Game"),
+    teams: Int,
+    playersInTeam: Int,
+    override val minPlayersToStart: Int = playersInTeam * 2
+) : AbstractGameMode() {
+    override val simpleName: String = "SkyWars"
+    override val name: Component = Component.text(simpleName)
+    override val description: Component = Component.text("The SkyWars Game")
     override val gameServer: (id: String, displayName: String) -> GameServer = { id, displayName ->
         SkyWarsServer(id, displayName)
-    },
-    override val minPlayersToStart: Int = 4, // TODO: make dynamic
-    override val maxPlayers: Int = 8 //TODO: make dynamic
-) : AbstractGameMode()
+    }
+    override val maxPlayers: Int = playersInTeam * teams
+}
